aa17a9c68def90d32e32775f319aaa4d34d0070b
Jethet <henriette.hettinga@protonmail.com>
2021-03-10 17:24:57 +0100

    Add express.urlencoded

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
 const PORT = 3000;
 
 // middleware
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static("public"));
 
+app.get("/", (req, res) => res.send("Hello world!"));
 
-app.listen(PORT, () => console.log("Server started on port 3000"))
+app.listen(PORT, () => console.log("Server started on port 3000"));
