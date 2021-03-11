f2720d0c69ec914894ef34e3d67a3f1ff119646d
Jethet <henriette.hettinga@protonmail.com>
2021-03-11 16:54:50 +0100

    Add pool config; add dotenv and .env variables

--- backend/routes.js
+++ backend/routes.js
@@ -1,2 +1,11 @@
 const express = require("express")
 const { Pool } = require("pg")
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: "localhost",
+  database: "info_inspiration",
+  password: process.env.PGPASSWORD,
+  port: 5432,
+});
+
