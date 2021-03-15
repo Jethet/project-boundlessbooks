a1b0207c752c6bfc62dc91809c4204daf0f1b300
Jethet <henriette.hettinga@protonmail.com>
2021-03-15 14:59:44 +0100

    Create database boundless_books; define endpoints

--- backend/routes.js
+++ backend/routes.js
@@ -17,7 +17,7 @@ Router.route("/")
     next();
   })
 
-  .get((req, res, next) => {
+  .get("/", (req, res, next) => {
     res.end(
       "When a GET request is made, then this " + "is the response sent to the client!"
     );
