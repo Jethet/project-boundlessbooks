3540105e4f3f1a30d1e5b77e93af2713966f5981
Jethet <henriette.hettinga@protonmail.com>
2021-04-13 19:13:26 +0200

    Create connector; add client.connect and .end to all queries

--- routes.js
+++ routes.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { Client } = require("pg");
+const client = require("connector.js")
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+// Change all queries: add client.connect and client.end
 
-client.connect();
 
 // define the home route
 router.get("/", function (req, res) {
@@ -19,11 +13,12 @@ router.get("/", function (req, res) {
 // AUTHORS
 // get all authors
 router.get("/authors", (req, res) => {
+  client.connect()
   client
     .query("SELECT * FROM authors;")
     .then((result) => res.json(result.rows))
     .catch((e) => console.error(e))
-    // .then(() => client.end());
+    .then(() => client.end());
 });
 
 // router.get("/authors", (req, res1) => {
@@ -40,37 +35,40 @@ router.get("/authors", (req, res) => {
 
 // get author by last name
 router.get("/authors/last", (req, res) => {
+  client.connect()
   const lastname = req.query.name;
   client
     .query("SELECT * FROM authors WHERE lastname=$1;", [lastname])
     .then((result) => res.json(result.rows))
     .catch((e) => console.error(e))
-    // .then(() => client.end())
+    .then(() => client.end())
 });
 
 // // get author by first name
 router.get("/authors/first", (req, res) => {
+  client.connect()
   const firstname = req.query.name;
   client
     .query("SELECT * FROM authors WHERE firstname=$1;", [firstname])
     .then((result) => res.json(result.rows))
     .catch((e) => console.error(e))
-    // .then(() => client.end())
+    .then(() => client.end())
 });
 
 // // get author by author id
 router.get("/authors/:id", (req, res) => {
-  // const authorId = parseInt(req.params.id);
+  client.connect()
   const authorId = req.params.id;
   client
     .query("SELECT * FROM authors WHERE id=$1;", [authorId])
     .then((result) => res.json(result.rows))
     .catch((e) => console.error(e))
-    // .then(() => client.end())
+    .then(() => client.end())
 });
 
 // // get author and their books by author id
 router.get("/authorbooks/:id", (req, res) => {
+  client.connect()
   const authorId = req.params.id;
   client
     .query(
@@ -78,7 +76,8 @@ router.get("/authorbooks/:id", (req, res) => {
       [authorId]
     )
     .then((result) => res.json(result.rows))
-    .catch((e) => console.error(e));
+    .catch((e) => console.error(e))
+    .then(() => client.end())
 });
 
 // add new author
@@ -124,41 +123,48 @@ router.get("/authorbooks/:id", (req, res) => {
 // BOOKS
 // get all books
 router.get("/books", (req, res) => {
+  client.connect()
   client
     .query("SELECT * FROM books;")
     .then((result) => res.json(result.rows))
-    .catch((e) => console.error(e));
+    .catch((e) => console.error(e))
+    .then(() => client.end())
+
 });
 
 // get book by word in title
 router.get("/books/title", (req, res) => {
-  const title = req.query.title;
+  client.connect()
+  const word = req.query.word;
   client
-    .query("SELECT * FROM books WHERE title LIKE $1;", [title])
+    .query("SELECT * FROM books WHERE title LIKE $%1%;", [word])
     .then((result) => res.json(result.rows))
-    .catch((e) => console.error(e));
+    .catch((e) => console.error(e))
+    .then(() => client.end())
 });
 
 // get book by book id
 router.get("/books/:id", (req, res) => {
+  client.connect()
   const bookId = req.params.id;
   client
     .query("SELECT * FROM books WHERE id=$1;", [bookId])
     .then((result) => res.json(result.rows))
-    .catch((e) => console.error(e));
+    .catch((e) => console.error(e))
+    .then(() => client.end())
 });
 
 // add new book title
-router.post("/books/new", (req, res) => {
-  const title = req.body.title;
-  const language = req.body.language;
-  // the authorId is not added to the books table
-  // const authorId = req.body.author_id
-  client
-    .query("INSERT INTO books (title, language) VALUES ($1, $2);", [title, language])
-    .then(() => res.send(`New book with title ${title} has been created.`))
-    .catch((e) => console.error(e));
-});
+// router.post("/books/new", (req, res) => {
+//   const title = req.body.title;
+//   const language = req.body.language;
+//   // the authorId is not added to the books table
+//   // const authorId = req.body.author_id
+//   client
+//     .query("INSERT INTO books (title, language) VALUES ($1, $2);", [title, language])
+//     .then(() => res.send(`New book with title ${title} has been created.`))
+//     .catch((e) => console.error(e));
+// });
 
 // // edit author firstname/lastname/booktitle ??
 // router.put("/books/:id", (req, res) => {
